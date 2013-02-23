fcf8de66069a48c42bebf6618e3ffc314fab1bf1
Yoshihiro Furukawa <yoshihiro.furukawa@nifty.com>
2013-02-23 23:53:18 +0900

    fix

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -4,10 +4,11 @@
  *  Created on: 2013/02/23
  *      Author: furukawayoshihiro
  */
-
+#include <iostream>
 #include "Cell.h"
 
-Cell::Cell() : isAlive_(false) {
+Cell::Cell() : isAlive_(false), UpperLeftCell_(NULL), UpperCell_(NULL), UpperRightCell_(NULL),
+		LeftCell_(NULL), RightCell_(NULL), LowerLeftCell_(NULL), LowerCell_(NULL), LowerRightCell_(NULL) {
 	// TODO Auto-generated constructor stub
 
 }
@@ -16,9 +17,68 @@ Cell::~Cell() {
 	// TODO Auto-generated destructor stub
 }
 
-Cell::Cell(bool isAlive) : isAlive_(isAlive) {
+void Cell::setLeftCell(Cell* leftCell) {
+	LeftCell_ = leftCell;
+}
+
+void Cell::setLowerCell(Cell* lowerCell) {
+	LowerCell_ = lowerCell;
+}
+
+void Cell::setLowerLeftCell(Cell* lowerLeftCell) {
+	LowerLeftCell_ = lowerLeftCell;
+}
+
+void Cell::setLowerRightCell(Cell* lowerRightCell) {
+	LowerRightCell_ = lowerRightCell;
+}
+
+void Cell::setRightCell(Cell* rightCell) {
+	RightCell_ = rightCell;
+}
+
+void Cell::setUpperCell(Cell* upperCell) {
+	UpperCell_ = upperCell;
+}
+
+void Cell::setUpperLeftCell(Cell* upperLeftCell) {
+	UpperLeftCell_ = upperLeftCell;
+}
+
+void Cell::setUpperRightCell(Cell* upperRightCell) {
+	UpperRightCell_ = upperRightCell;
 }
 
 bool Cell::isAlive() {
 	return isAlive_;
+}
+
+int Cell::checkNumberOfNeiborhoodIsAlive() {
+	int count(0);
+	if(UpperLeftCell_->isAlive_) {
+		++count;
+	}
+	if(UpperCell_->isAlive_) {
+		++count;
+	}
+	if(UpperRightCell_->isAlive_) {
+		++count;
+	}
+	if(LeftCell_->isAlive_) {
+		++count;
+	}
+	if(RightCell_->isAlive_) {
+		++count;
+	}
+	if(LowerLeftCell_->isAlive_) {
+		++count;
+	}
+	if(LowerCell_->isAlive_) {
+		++count;
+	}
+	if(LowerRightCell_->isAlive_) {
+		++count;
+	}
+
+	return count;
 }
